8f3a1ff7c6bd5b67dd1d9eb320998b3fe3d33974
Mampionona <devke03@gmail.com>
2019-01-24 11:55:11 +0300

    favicon

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title(''); ?></title>
-	<?php if (isset($themeum['favicon'])) : ?>
+	<?php if (isset($themeum['favicon']) && !empty($themeum['favicon'])) : ?>
 		<link rel="shortcut icon" href="<?php echo $themeum['favicon']; ?>" type="image/x-icon"/>
 	<?php else : ?>
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri().'/images/plus.png' ?>" type="image/x-icon"/>
