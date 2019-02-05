99af9094634b3a4a6bdff2e9c45f1acff5e3b21a
Mampionona <devke03@gmail.com>
2019-02-05 17:22:27 +0300

    image de la home en asynchrone

--- assets/js/templates/home.js
+++ assets/js/templates/home.js
@@ -1,1 +1,13 @@
-// homepage
+(function(w, d, undefined) {
+  if (d.body.className.match(/home/)) {
+    const main = d.getElementById('ajax-content')
+    if (main) {
+      const image = new Image()
+      image.addEventListener('load', () => {
+        main.style.backgroundImage = `url('${main.dataset.backgroundImage}')`
+      })
+      
+      image.src = main.dataset.backgroundImage
+    }
+  }
+} (window, document))
