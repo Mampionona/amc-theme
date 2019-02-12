0cb3163fc22ead6ff983e100610d5e26397c2cfb
Mampionona <devke03@gmail.com>
2019-02-12 13:09:26 +0300

    [home] background-image

--- assets/js/templates/home.js
+++ assets/js/templates/home.js
@@ -2,12 +2,7 @@
   if (d.body.className.match(/home/)) {
     const main = d.getElementById('ajax-content')
     if (main) {
-      const image = new Image()
-      image.addEventListener('load', () => {
-        main.style.backgroundImage = `url('${main.dataset.backgroundImage}')`
-      })
-      
-      image.src = main.dataset.backgroundImage
+      main.style.backgroundImage = `url('${main.dataset.backgroundImage}')`
     }
   }
 } (window, document))
