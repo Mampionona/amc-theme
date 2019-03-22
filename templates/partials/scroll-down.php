8075b6d7de6a729280356d9c97b5451a296948c3
Mampionona <devke03@gmail.com>
2019-03-22 12:32:47 +0300

    bouton scroll down traduit

--- templates/partials/scroll-down.php
+++ templates/partials/scroll-down.php
@@ -1,6 +1,6 @@
 <div class="text-center scroll-down-wrap">
     <button class="btn scroll-down">
-        <span class="d-block"><?php _e('Scroll down', AMC_TEXTDOMAIN); ?></span>
+        <span class="d-block"><?php _e('Basculer vers le bas', AMC_TEXTDOMAIN); ?></span>
         <span class="icon-wrap">
         </span>
     </button>
