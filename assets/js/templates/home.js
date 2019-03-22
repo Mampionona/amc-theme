c5842ca85f748c5f0c6a3125ad6b9e7d46de01c7
Mampionona <devke03@gmail.com>
2019-03-22 16:14:28 +0300

    Afficher les éléments sur la page d'accueil au lieu de les replier dans le bouton burger

--- assets/js/templates/home.js
+++ assets/js/templates/home.js
@@ -1,8 +1,10 @@
 (function(w, d, undefined) {
-  if (d.body.className.match(/home/)) {
+  if (d.body.classList.contains('home')) {
     const main = d.getElementById('ajax-content');
     if (main) {
       main.style.backgroundImage = `url('${main.dataset.backgroundImage}')`;
     }
+
+    d.body.classList.add('menu-in');
   }
 } (window, document));
