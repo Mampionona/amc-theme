b6a5ff86b7852aaba8522ab17fb3dca8c1adb9ea
Mampionona <devke03@gmail.com>
2019-04-08 09:43:14 +0300

    bouton Basculer vers le bas => lire plus

--- templates/partials/scroll-down.php
+++ templates/partials/scroll-down.php
@@ -1,6 +1,6 @@
 <div class="text-center scroll-down-wrap">
     <button class="btn scroll-down">
-        <span class="d-block"><?php _e('Basculer vers le bas', AMC_TEXTDOMAIN); ?></span>
+        <span class="d-block"><?php _e('Lire plus', AMC_TEXTDOMAIN); ?></span>
         <span class="icon-wrap">
         </span>
     </button>
